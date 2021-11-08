1b652fcc5b1de8d4a1246ccb12432574815f7b3e
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-08 20:35:19 +0100

    Create restaurants_controller

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,21 @@
+class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show]
+  def index
+    @restaurants = Restaurant.all
+    render json: @restaurants
+  end
+
+  def import
+    Restaurant.csv_import
+  end
+
+  def show
+    render json: @restaurant
+  end
+
+  private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
