aa6d548db88457ed42a0cb49d60b3bf3af5e44a2
enaburekhan <ericenaburekhan2018@gmail.com>
2021-11-09 16:48:19 +0100

    Refactor association in favorite_restaurant model

--- spec/models/favorite_restaurant_spec.rb
+++ spec/models/favorite_restaurant_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe FavoriteRestaurant, type: :model do
   # Association test
-  it { should have_many(:restaurants) }
-  it { should have_many(:collections) }
+  it { should belong_to(:restaurant) }
+  it { should belong_to(:collection) }
 end
